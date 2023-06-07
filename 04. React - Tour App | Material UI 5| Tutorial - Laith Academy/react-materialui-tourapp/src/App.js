ffb1d9a4ec6c64c514ee474e666ba175041e4a95
Daniel Bauer <dan717@gmx.de>
2023-06-07 10:17:10 +0200

    done basic card

--- 04. React - Tour App | Material UI 5| Tutorial - Laith Academy/react-materialui-tourapp/src/App.js
+++ 04. React - Tour App | Material UI 5| Tutorial - Laith Academy/react-materialui-tourapp/src/App.js
@@ -1,13 +1,48 @@
 import "./App.css";
 import Container from "@mui/material/Container";
+import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
 
 import TourCard from "./components/TourCard";
+
+import Schwarzwald from "./assets/images/©DB_200705_Schwarzwald_045.jpg";
+import Kroatien from "./assets/images/©DB_200726_Kroatien_470.jpg";
+import Frankreich from "./assets/images/©DB_200831_02_Frankreich_149.jpg";
+import Portugal from "./assets/images/©DB_200831_04_Portugal_014.jpg";
 
 function App() {
 	return (
 		<div className="App">
 			<Container>
-				<TourCard />
+				<Grid container spacing={2}>
+					<TourCard
+						img={Schwarzwald}
+						cardTopic="Im Schwarzwald"
+						rating={4}
+                        reviews={354}
+                        price= "274"
+					/>
+					<TourCard
+						img={Kroatien}
+						cardTopic="In Kroatien"
+						rating={4.5}
+                        reviews={274}
+                        price= "573"
+					/>
+					<TourCard
+						img={Frankreich}
+						cardTopic="In Frankreich"
+						rating={4}
+                        reviews={1472}
+                        price= "974"
+					/>
+					<TourCard
+						img={Portugal}
+						cardTopic="In Portugal"
+						rating={4.5}
+                        reviews={2742}
+                        price= "482"
+					/>
+				</Grid>
 			</Container>
 		</div>
 	);
--- 04. React - Tour App | Material UI 5| Tutorial - Laith Academy/react-materialui-tourapp/src/components/TourCard.js
+++ 04. React - Tour App | Material UI 5| Tutorial - Laith Academy/react-materialui-tourapp/src/components/TourCard.js
@@ -1,8 +1,77 @@
 import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import { AccessTime } from "@mui/icons-material";
+import Rating from "@mui/material/Rating";
 
-
-const TourCard = () => {
-	return <Paper elevation={2} >Tour Card</Paper>;
+const TourCard = (props) => {
+	console.log(props);
+	return (
+		<Grid xs={3}>
+			<Paper elevation={2} xs={3}>
+				<img src={props.img} className="img" alt="" />
+				<Box paddingX={1}>
+					<Typography variant="subtitle1" component="h2">
+						{props.cardTopic}
+					</Typography>
+					<Box
+						sx={{
+							display: "flex",
+							alignItems: "center",
+						}}
+					>
+						<AccessTime sx={{ width: 12.5 }} />
+						<Typography
+							variant="body2"
+							component="p"
+							marginLeft={0.5}
+						>
+							5 hours
+						</Typography>
+					</Box>
+					<Box
+						sx={{
+							display: "flex",
+							alignItems: "center",
+						}}
+                        marginTop={3}
+					>
+						<Rating
+							name="half-rating-read"
+							value={props.rating}
+							precision={0.5}
+							readOnly
+                            size="small"
+						/>
+					<Typography
+							variant="body2"
+							component="p"
+							marginLeft={0.5}
+						>
+							{props.rating}
+						</Typography>
+                        <Typography
+							variant="body3" // Das wird als custom variant angelegt. body2 ist das kleinste, wiir müssen kleiner
+							component="p"
+							marginLeft={0.5}
+						>
+							({props.reviews} reviews)
+						</Typography>
+					</Box>
+                    <Box>
+                    <Typography
+							variant="h6"
+							component="h3"
+							marginTop={0}
+						>
+							From € {props.price}
+						</Typography>
+                    </Box>
+				</Box>
+			</Paper>
+		</Grid>
+	);
 };
 
 export default TourCard;
